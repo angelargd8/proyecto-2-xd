8d1d0dbe0399a18c2fc09c01ef1831974ba093ff
Geraxxx5 <gerax.pineda@gmail.com>
2023-05-21 15:14:11 -0600

    Se agrego el apartado de neo4j con el inicio de sesion

--- Programa Recomendaciones/main.py
+++ Programa Recomendaciones/main.py
@@ -1,12 +1,43 @@
 from flask import Flask, request, render_template
+from neo4j import GraphDatabase
+
+class Neo4JExample:
+
+    def __init__(self, uri, user, password):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        self.driver.close()
+
+    def print_Users(self,correo):
+        with self.driver.session() as session:
+            entrada = session.execute_write(self.showUsers,correo)
+        return entrada
+    
+    @staticmethod
+    def showUsers(tx,correo):
+        result = tx.run('match (n:Estudiante{correo: "'+correo+'"}) return n.correo, n.contra')
+        print(result)
+        ret = []
+        for record in result:
+            ret.append(record["n.correo"])
+            ret.append(record["n.contra"])
+        if(len(ret) == 0):
+            ret.append("No existe")
+        return ret
+
+
+
 #Aqui tienen que cambiar a su ruta de la carpeta raíz
-app = Flask(__name__,template_folder= 'C:\\xampp\\htdocs\\proyecto-2-xd\\Programa Recomendaciones') #aqui se empieza a crear la aplicacion
+app = Flask(__name__,template_folder= 'C:\\Users\\USUARIO\\Desktop\\Proyecto2Github\\proyecto-2-xd\\Programa Recomendaciones') #aqui se empieza a crear la aplicacion
+BD = Neo4JExample("bolt://localhost:7687", "neo4j", "12345678")
 
 @app.route('/') #se define un temporador para la ruta principal '/login'
 
 def inicio():
     #renderizamos la plantilla, formulario html
     return render_template('index.html')
+
 
 #se define el route con el metodo post
 @app.route('/form', methods=['POST'])
@@ -16,8 +47,18 @@ def Form():
     nombre = request.form['nombre']
     contrasena = request.form['contrasena']
 
-    #retornamos una respuesta
-    return "<h1>binvenido "+ nombre+"</h1>"
+    ret = BD.print_Users(nombre)
+    
+    if(ret[0] == "No existe"):
+        #Aqui se tiene que poner un alert o algo no se como se hace
+        return "<h1>binvenido El usuario ingresado no existe </h1>"
+    else:
+        if(ret[0] == nombre and contrasena == ret[1]):
+            #Tal ve aqui darle acceso a alguna otra pantalla como un menu
+            return "<h1> Credenciales correctas </h1>"
+        else:
+            return "<h1>Error en el usuario o contraseña </h1>"
+
     #return render_template('form.html', nombre=nombre, contrasena=contrasena)
 
     #redireccionar
@@ -26,3 +67,5 @@ def Form():
 if __name__ == '__main__':
     #se inicia la aplicacion en modo debug
     app.run(debug=True)
+
+
