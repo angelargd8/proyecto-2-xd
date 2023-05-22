841f3f38f31159231597089475d33828973374e2
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2023-05-21 21:16:17 -0600

    arreglo de imagenes en MenuPrincipal para el servidor

--- Programa Recomendaciones/main.py
+++ Programa Recomendaciones/main.py
@@ -30,7 +30,8 @@ class Neo4JExample:
 
 #Aqui tienen que cambiar a su ruta de la carpeta raíz
 #path Gerax: C:\\Users\\USUARIO\\Desktop\\Proyecto2Github\\proyecto-2-xd\\Programa Recomendaciones
-app = Flask(__name__,template_folder= 'C:\\Users\\USUARIO\\Desktop\\Proyecto2Github\\proyecto-2-xd\\Programa Recomendaciones') #aqui se empieza a crear la aplicacion
+#path Angela: C:\\xampp\\htdocs\\proyecto-2-xd\\Programa Recomendaciones
+app = Flask(__name__,template_folder= 'C:\\xampp\\htdocs\\proyecto-2-xd\\Programa Recomendaciones') #aqui se empieza a crear la aplicacion
 BD = Neo4JExample("bolt://localhost:7687", "neo4j", "12345678")
 #neo4j,neo4j
 
